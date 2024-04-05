3f0e4dea17481c7bf415c2fd2bbaaba865d9b679
AnnaKolesnichenko <117039891+AnnaKolesnichenko@users.noreply.github.com>
2024-04-05 11:11:12 +0300

    Update api-services.js

--- src/services/api-services.js
+++ src/services/api-services.js
@@ -2,13 +2,12 @@ import axios from 'axios';
 
 const API_URL =
   'https://medic-delivery-app-default-rtdb.europe-west1.firebasedatabase.app';
-const API_FIREB = 'AIzaSyAj-Zbx2MFCukdvQq2XoOQVnV2MBYKniOg';
 
 const GOOGLE_API_KEY = 'AIzaSyAUj3Qur_NFCB-mhi6TL6M8JCFXWxQI2V8';
 
 export const getDrug24 = async () => {
   try {
-    const response = await fetch(`${API_URL}/drug24.json?auth=${API_FIREB}`);
+    const response = await fetch(`${API_URL}/drug24.json`);
     if (!response.ok) {
       throw new Error('Failed to fetch data');
     }
@@ -22,7 +21,7 @@ export const getDrug24 = async () => {
 
 export const getEDrug = async () => {
   try {
-    const response = await fetch(`${API_URL}/e-drug.json?auth=${API_FIREB}`);
+    const response = await fetch(`${API_URL}/e-drug.json`);
     if (!response.ok) {
       throw new Error('Failed to fetch data');
     }
@@ -36,7 +35,7 @@ export const getEDrug = async () => {
 
 export const getMedical = async () => {
   try {
-    const response = await fetch(`${API_URL}/medical.json?auth=${API_FIREB}`);
+    const response = await fetch(`${API_URL}/medical.json`);
     if (!response.ok) {
       throw new Error('Failed to fetch data');
     }
@@ -50,7 +49,7 @@ export const getMedical = async () => {
 
 export const getPharmacy = async () => {
   try {
-    const response = await fetch(`${API_URL}/pharmacy.json?auth=${API_FIREB}`);
+    const response = await fetch(`${API_URL}/pharmacy.json`);
     if (!response.ok) {
       throw new Error('Failed to fetch data');
     }
@@ -64,7 +63,7 @@ export const getPharmacy = async () => {
 
 export const getStayWell = async () => {
   try {
-    const response = await fetch(`${API_URL}/staywell.json?auth=${API_FIREB}`);
+    const response = await fetch(`${API_URL}/staywell.json`);
     if (!response.ok) {
       throw new Error('Failed to fetch data');
     }
@@ -81,7 +80,7 @@ export const addItemsToDB = data => {
 };
 
 export const getOrderInfo = async () => {
-  const orderInfo = await fetch(`${API_URL}/userData.json?auth=${API_FIREB}`);
+  const orderInfo = await fetch(`${API_URL}/userData.json`);
   const res = await orderInfo.json();
 
   return res;
